046e491818803a1db8fc421a55e7313ca95283f3
John Luis <johnluisvmayor@iskolarngbayan.pup.edu.ph>
2026-09-04 18:48:05 +0800

    Initial commit: Created starter code py file

---
+++ project_activities/student_grade_analyzer.py
@@ -0,0 +1,14 @@
+# ============================================================
+# CMPE 201 Section2-2 - Data Structures and Algorithm
+# FINAL CHALLENGE: Student Grade Analyzer
+# Submitted by GROUP 5
+# Ferrer, Angelo Terrence D.
+# Flores, Prince Menard T.
+# Guillen, Rod John F.
+# Mayor, John Luis V.
+# Pagdanganan, Kryzle Camille S.
+# ============================================================
+
+def display_students(students):
+    """Displays all students with their assigned index and grade."""
+    pass
