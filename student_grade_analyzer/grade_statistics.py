59c25cd41fac989d949d93d1be3b4a1d9bebf1fa
olegbyte-del <angeloferrer5436@gmail.com>
2026-09-05 22:32:59 +0800

    refactor: use list comprehension for grade validation

--- student_grade_analyzer/grade_statistics.py
+++ student_grade_analyzer/grade_statistics.py
@@ -1,17 +1,29 @@
+import pandas as pd
+import numpy as np
+
 class GradeStatistics:
     
     def validate(self, grades):
-        valid_grades = []
-        
+
         for grade in grades:
             
             if grade < 0 or grade > 100:
-                return "Invalid Grade"
-            
-            if grade < 75 and grade > 0: 
-                grade = 75 
-            
-            valid_grades.append(grade)
+                return np.nan
+    
+        valid_grades =  [
+            75 if grade < 75 and grade > 0 else grade
+            for grade in grades
+        ]
         
         return valid_grades
-            
+    
+    def highes(self, grades):
+        grades = self.validate(grades).dropna()
+        
+        return grades.max()
+    
+    def lowest(self, grades):
+        pass
+    
+    def average(self, grades):
+        pass
